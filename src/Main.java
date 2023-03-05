1a0bb84fdc31b2c5e6ee4ae96d73895a6a484737
Mark Owings <owingsmk@mail.uc.edu>
2023-03-05 10:39:38 -0500

    First Commit and try to get it right

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,40 @@
-import java.util.Random
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        public static int bank = 100;
-        public static int bet = 10;
-        public static int point = 0;
-        public static Random dice1 = new Random();
-        public static Random dice2 = new Random();
-        public static int total;
+        int bank = 100; //variables for the program
+        int bet = 10;
+        int point = 0;
+        Random dice1 = new Random();
+        Random dice2 = new Random();
+        int total;
 
-        public static void FirstRoll(){
+            int d1Value, d2Value; //setting the variables for the formula for program
+            do {
+                d1Value = dice1.nextInt(6) + 1;
+                d2Value = dice2.nextInt(6) + 1;
+                total = d1Value + d2Value;
+                System.out.printf("%d and %d equals %d\n", d1Value, d2Value, total); //formulas
+                switch (total){
+                    case 2:
+                    case 3:
+                    case 12:
+                        System.out.println(total + " you lose");
+                        bank -= bet;
+                        break;
+                    case 7:
+                    case 11:
+                        System.out.println(total + " you win");
+                        bank += bet;
+                        break;
+                    default:
+                        point = total;
+                        System.out.println("Point " + point);
+                        break;
+                }
+            }while (point ==0);
 
-        }
+
 
     }
 }
