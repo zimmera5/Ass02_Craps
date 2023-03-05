b2c4060e5bc1ea96de079dfcb79d600a04ca625f
Mark Owings <owingsmk@mail.uc.edu>
2023-03-05 10:20:33 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Random
+public class Main {
+    public static void main(String[] args) {
+
+        public static int bank = 100;
+        public static int bet = 10;
+        public static int point = 0;
+        public static Random dice1 = new Random();
+        public static Random dice2 = new Random();
+        public static int total;
+
+        public static void FirstRoll(){
+
+        }
+
+    }
+}
